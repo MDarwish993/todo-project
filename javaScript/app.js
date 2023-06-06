85df0d460c8ec19a062bc62b2f5c3e8a584bbfe5
MDarwish993 <m.darwish993@hotmail.com>
2023-06-06 08:27:18 +0000

    Lab06Solution

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,22 +1,59 @@
+'use strict';
+
+
+
+
 let inputName = prompt("Please enter your name", "mohammad Darwish");
 
 let gender = prompt("Please enter your gender", "male");
-    if(gender=="male" || gender=="female"){
-        gender=gender;
-    }else{
+    while (!(gender=="male" || gender=="female")) {
         gender= prompt("Please enter your gender ,please be either male or female", "male");
     }
 
 
 let age = prompt("Please enter your Age", 25);
-    if(age >= 0){
-        age=age;
-    }else{
-        window.alert("the age should be more than zero!");
+    while (age <= 0) {
         age = prompt("Please enter your Age again, should be above zero.", 25);
     }
+   
 
-let confirmation;
+//Lab Six Solution Start From Here 
+
+let QuestionAnswers=[];
+
+let QuestionAnswersCheck=(answer)=>{
+    while (!(answer== "Yes" || answer== "No" || answer== "")) {
+        answer=prompt("your Answer should be either Yes ,No Or empty");
+    }
+
+    if(answer == "Yes" || answer == "No" ){
+        QuestionAnswers.push(answer)
+    }else{
+        QuestionAnswers.push("invalid")
+    }
+
+}
+
+let PrintQuestionAnswers=(arr)=>{
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);  
+    }
+}
+
+let firstQuestion=prompt("Are you jordanian?");
+    QuestionAnswersCheck(firstQuestion);
+    let secondQuestion=prompt("Are you live in amman?");
+    QuestionAnswersCheck(secondQuestion);
+    let thiredQuestion=prompt("Are you developer?");
+    QuestionAnswersCheck(thiredQuestion);
+
+    PrintQuestionAnswers(QuestionAnswers);
+
+/*
+I fixed Confirmation Box From Lab Five At The End 
+So We Can Include The Lab Six Questions With The Confirmation Box
+*/
+    let confirmation;
     if (confirm("confirm if you want to skip the welcoming message.")) {
         confirmation = "You pressed OK!";
       } else if (gender=="male") {
